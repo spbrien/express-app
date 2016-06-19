7322ba39f7fcbcc5d6282ff20897ecc4640f85c8
Steven Brien <spbrien@gmail.com>
2016-06-19 15:25:55 -0700

    Added some notes in comments

--- application/index.js
+++ application/index.js
@@ -3,13 +3,15 @@ const R = require('ramda')
 
 function factory(app, port) {
   return (config) => {
+    // Create Schema Routes
     R.map(model => {
-      app.get(`/${model.name}`, (req, res) => {
+      app.get(`/api/v1/${model.name}`, (req, res) => {
         res.send(model._schema)
       })
     }, config.schema)
-   
-   app.listen(port, () => console.log(`Running on port ${port}\n`))
+  
+    // Listen
+    app.listen(port, () => console.log(`Running on port ${port}\n`))
   }
 }
 
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 'use strict'
+
+// Modules
 const app = require('express')()
 const morgan = require('morgan')
 const config = require('./config')
 const factory = require('./application')
+
+// Settings
 const port = 8080
 
+// Init Logging
 app.use(morgan('dev'))
+
+// Init Application
 factory(app, port)(config)
