c856c8ae22c9437aa136eb7bb751d057c75494f7
Brendan Bowidas <brendan@machine.andrew.response.private>
2016-07-22 09:03:18 -0400

    fixed bulk insert not adding meta info for each item

--- config/db/methods/insert.js
+++ config/db/methods/insert.js
@@ -1,10 +1,19 @@
 const r = require('rethinkdb')
 const md5 = require('md5')
-
+/* eslint-disable consistent-return */
 function insert(tableName, data, connection) {
-  data._created = r.now()
-  data._etag = md5(JSON.stringify(data))
-  return r.table(tableName).insert(data, { returnChanges: true }).run(connection)
+  let iterations = 0
+  if (!Array.isArray(data)) {
+    data = [data]
+  }
+  for (const item of data) {
+    iterations++
+    item._created = r.now()
+    item._etag = md5(JSON.stringify(data))
+    if (iterations === data.length) {
+      return r.table(tableName).insert(data, { returnChanges: true }).run(connection)
+    }
+  }
 }
 
 module.exports = insert
